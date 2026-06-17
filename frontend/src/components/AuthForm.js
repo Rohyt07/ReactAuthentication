6acf56a5efe52e76ec2b5c9e73cd5e333e56b3ca
rohit khare <rohitmein27@gmail.com>
2026-06-17 22:54:30 +0530

    Query Parameters

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,19 +1,19 @@
 import { useState } from 'react';
-import { Form } from 'react-router-dom';
+import { Form, useSearchParams, Link } from 'react-router-dom';
 
 import classes from './AuthForm.module.css';
 
 function AuthForm() {
-  const [isLogin, setIsLogin] = useState(true);
+  const [searchParams] = useSearchParams();
 
-  function switchAuthHandler() {
-    setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
-  }
+  const isLogin = searchParams.get('mode') === 'login'
+
+
 
   return (
     <>
       <Form method="post" className={classes.form}>
-        <h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
+        <h1>{isLogin ? 'Log in' : 'SignUP'}</h1>
         <p>
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" required />
@@ -23,9 +23,11 @@ function AuthForm() {
           <input id="password" type="password" name="password" required />
         </p>
         <div className={classes.actions}>
-          <button onClick={switchAuthHandler} type="button">
-            {isLogin ? 'Create new user' : 'Login'}
-          </button>
+
+          <Link to={`?mode=${isLogin?'signup' : 'login'}`}>
+            {isLogin ? 'SignUp' : 'Login'}
+          </Link>
+
           <button>Save</button>
         </div>
       </Form>
